746038a29f3d2d456be5490ae7f2394a6948a76b
leaffan <leaffan@gmx.net>
2017-01-11 03:34:17 +0100

    Update player definition

--- db/player.py
+++ db/player.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from .common import Base, session_scope
+
+
+class Player(Base):
+    __tablename__ = 'players'
+    __autoload__ = True
+
+    def __init__(self):
+        pass
