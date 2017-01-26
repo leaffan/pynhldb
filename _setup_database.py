d8f14d6452b89283d0b1e3a7d161d0c12e742a4f
leaffan <leaffan@gmx.net>
2017-01-26 16:45:52 +0100

    Include retrieval of player seasons and individual data

--- _setup_database.py
+++ _setup_database.py
@@ -4,12 +4,22 @@
 from setup.create_teams import migrate_teams
 from setup.create_divisions import create_divisions
 from setup.create_players import migrate_players
+from setup.create_player_seasons import create_player_seasons
+from setup.create_player_seasons import create_player_data
+
+from utils import prepare_logging
+prepare_logging(log_types=['file', 'screen'])
 
 
 if __name__ == '__main__':
+
     # migrating teams from json file to database
     migrate_teams(simulation=True)
     # creating divisions from division configuration file
     create_divisions(simulation=True)
     # migrating players from json file to database
     migrate_players(simulation=True)
+    # retrieving player season statistics for all players in database
+    create_player_seasons(simulation=False)
+    # retrieving individual player data for all players in database
+    create_player_data(simulation=False)
--- setup/create_player_seasons.py
+++ setup/create_player_seasons.py
@@ -35,3 +35,25 @@ def create_player_seasons(simulation=False):
                     print("Concurrent task generated an exception: %s" % e)
 
     logger.info("+ %d statistics items retrieved overall" % plr_season_count)
+
+
+def create_player_data(simulation=False):
+
+    data_retriever = PlayerDataRetriever()
+
+    with session_scope() as session:
+
+        players = session.query(Player).all()[:]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as threads:
+            future_tasks = {
+                threads.submit(
+                    data_retriever.retrieve_player_data,
+                    player.player_id, simulation
+                ): player for player in players
+            }
+            for future in concurrent.futures.as_completed(future_tasks):
+                try:
+                    pass
+                except Exception as e:
+                    print("Concurrent task generated an exception: %s" % e)
