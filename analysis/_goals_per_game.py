a291352718eb02888d7bffb9f4256f4127eb83c6
leaffan <leaffan@gmx.net>
2017-01-30 15:19:29 +0100

    Include subtraction of shootout goals from totals

--- analysis/_goals_per_game.py
+++ analysis/_goals_per_game.py
@@ -16,7 +16,7 @@ overall_sum_goals_scored = 0
 games_goals_per_year = defaultdict(dict)
 goal_leaders = set()
 
-for year in range(1918, 2017)[:10]:
+for year in range(1918, 2017)[:]:
 
     # skipping season completely lost to a lockout
     if year == 2005:
@@ -47,6 +47,9 @@ for year in range(1918, 2017)[:10]:
     season_games_played = [
         int(x) for x in team_stats.xpath(
             "//tbody/tr/td[@data-stat='games']/text()")]
+    season_shootout_wins = [
+        int(x) for x in team_stats.xpath(
+            "//tbody/tr/td[@data-stat='wins_shootout']/text()")]
     # retrieving goals scored by each team as list
     season_goals_scored = [
         int(x) for x in team_stats.xpath(
@@ -57,15 +60,18 @@ for year in range(1918, 2017)[:10]:
     # summing up games played in current season
     season_games_played = int(sum(season_games_played) / 2)
     overall_sum_games_played += season_games_played
+    # summing up shootout wins in current season
+    season_shootout_wins = sum(season_shootout_wins)
     # summing up goals scored in current season
-    season_goals_scored = sum(season_goals_scored)
+    # subtracting number of shootout wins as these aren't official goals
+    season_goals_scored = sum(season_goals_scored) - season_shootout_wins
     overall_sum_goals_scored += season_goals_scored
 
     # adding per year data to result dictionary
-    games_goals_per_year[year]['season'] = season
-    games_goals_per_year[year]['games'] = season_games_played
-    games_goals_per_year[year]['goals'] = season_goals_scored
-    games_goals_per_year[year]['goals_per_game'] = round(
+    games_goals_per_year[season]['year'] = year
+    games_goals_per_year[season]['games'] = season_games_played
+    games_goals_per_year[season]['goals'] = season_goals_scored
+    games_goals_per_year[season]['goals_per_game'] = round(
         season_goals_scored / season_games_played, 2)
 
     print("%s: %d games played, %d goals scored, %0.2f goals per game" % (
@@ -76,9 +82,9 @@ for year in range(1918, 2017)[:10]:
 overall_goals_per_game = round(
     overall_sum_goals_scored / overall_sum_games_played, 2)
 
-print(
+print("overall: %d games played, %d goals scored, %0.2f goals per game" % (
     overall_sum_games_played, overall_sum_goals_scored,
-    round(overall_sum_goals_scored / overall_sum_games_played, 2))
+    overall_goals_per_game))
 
 # calculating adjustment factor for each registered season
 for year in sorted(games_goals_per_year.keys()):
@@ -86,8 +92,10 @@ for year in sorted(games_goals_per_year.keys()):
         year]['adjustment_factor'] = round((
             overall_goals_per_game / (
                 games_goals_per_year[year]['goals_per_game'])), 4)
-    print(games_goals_per_year[year]['season'])
-    print(games_goals_per_year[year]['adjustment_factor'])
 
-open(r"d:\nhl_games_per_season.json", 'w').write(
+games_goals_per_year['overall']['games'] = overall_sum_games_played
+games_goals_per_year['overall']['goals'] = overall_sum_goals_scored
+games_goals_per_year['overall']['goals_per_game'] = overall_goals_per_game
+
+open(r"nhl_games_per_season.json", 'w').write(
     json.dumps(games_goals_per_year, sort_keys=True, indent=2))
