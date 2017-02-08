bb1913622a869059937ad5d952edf4eccc77046e
leaffan <leaffan@gmx.net>
2017-02-08 04:19:31 +0100

    Define six player ids for testing contract data retrieval

--- setup/create_player_seasons.py
+++ setup/create_player_seasons.py
@@ -66,20 +66,10 @@ def create_player_contracts(simulation=False):
     """
     data_retriever = PlayerDataRetriever()
 
-    # player_id = 8467329
-    # # player_id = 8469707  # Parenteau -> alternative Namen
-    # player_id = 8470595
-    # player_id = 8477939
-
-    player_ids = [8467329, 8470595, 8477939, 8467950, 8462042]
+    player_ids = [8467329, 8470595, 8477939, 8467950, 8462042, 8467496]
 
     for player_id in player_ids:
         data_retriever.retrieve_raw_contract_data(player_id)
-
-    # with session_scope() as session:
-    #     players = session.query(Player).all()[:]
-
-
 
     #     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as threads:
     #         future_tasks = {
