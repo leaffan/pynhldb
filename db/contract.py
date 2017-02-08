3173703bd4c3707a322f557a9e8c8822d7ef2c92
leaffan <leaffan@gmx.net>
2017-02-08 04:12:21 +0100

    Include find method for contract item

--- db/contract.py
+++ db/contract.py
@@ -3,12 +3,37 @@
 
 from .common import Base, session_scope
 
+from sqlalchemy import and_
+
 
 class Contract(Base):
     __tablename__ = 'contracts'
     __autoload__ = True
 
+    STANDARD_ATTRS = [
+        'signing_team_id', 'signing_date', 'length', 'value', 'type',
+        'expiry_status', 'source', 'start_season', 'end_season',
+        'cap_hit_percentage', 'bought_out'
+        ]
 
-def __init__(self, player_id, contract_data):
+    def __init__(self, player_id, contract_data_dict):
 
+        self.player_id = player_id
+        for attr in self.STANDARD_ATTRS:
+            if attr in contract_data_dict:
+                setattr(self, attr, contract_data_dict[attr])
 
+    @classmethod
+    def find(self, player_id, start_season, end_season):
+        with session_scope() as session:
+            try:
+                contract = session.query(Contract).filter(
+                    and_(
+                        Contract.player_id == player_id,
+                        Contract.start_season == start_season,
+                        Contract.end_season == end_season
+                    )
+                ).one()
+            except:
+                contract = None
+            return contract
