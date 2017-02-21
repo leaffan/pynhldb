ecb8110813d13f5db001c20b872c8c93fd8d0595
leaffan <leaffan@gmx.net>
2017-02-21 01:45:48 +0100

    Add additional find method and comparison operators for player draft items

--- db/player_draft.py
+++ db/player_draft.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from sqlalchemy import and_
+
 from .common import Base, session_scope
 
 
@@ -17,7 +19,22 @@ class PlayerDraft(Base):
             self.draft_type = dft_type
 
     @classmethod
-    def find_by_player_id(self, player_id):
+    def find(cls, player_id, team_id, year):
+        with session_scope() as session:
+            try:
+                plr_draft = session.query(PlayerDraft).filter(
+                    and_(
+                        PlayerDraft.player_id == player_id,
+                        PlayerDraft.team_id == team_id,
+                        PlayerDraft.year == year
+                    )
+                ).one()
+            except:
+                plr_draft = None
+            return plr_draft
+
+    @classmethod
+    def find_by_player_id(cls, player_id):
         with session_scope() as session:
             try:
                 plr_draft = session.query(PlayerDraft).filter(
@@ -34,12 +51,26 @@ class PlayerDraft(Base):
 
     def __eq__(self, other):
         return (
-            (self.team_id, self.year, self.round,
-             self.overall, self.draft_type
-            ) ==
-            (other.team_id, other.year, other.round,
-             other.overall, other.draft_type
-            ))
+            self.team_id, self.year, self.round,
+            self.overall, self.draft_type
+            ) == (
+            other.team_id, other.year, other.round,
+            other.overall, other.draft_type)
 
     def __ne__(self, other):
         return not self == other
+
+    def __gt__(self, other):
+        if self.year == other.year:
+            if self.overall >= other.overall:
+                return True
+            else:
+                return False
+        else:
+            if self.year > other.year:
+                return True
+            else:
+                return False
+
+    def __lt__(self, other):
+        return not self.__gt__(other)
