f8a2a826ede704b3ad9169e15894f71756e2a4d2
leaffan <leaffan@gmx.net>
2017-03-05 23:05:45 +0100

    Add update and comparison functions

--- db/game.py
+++ db/game.py
@@ -25,6 +25,17 @@ class Game(Base):
                 setattr(self, attr, game_data_dict[attr])
 
     @classmethod
+    def find_by_id(self, game_id):
+        with session_scope() as session:
+            try:
+                g = session.query(Game).filter(
+                    Game.game_id == game_id
+                ).one()
+            except:
+                g = None
+        return g
+
+    @classmethod
     def find(self, date, road_team, home_team):
         with session_scope() as session:
             try:
@@ -37,6 +48,36 @@ class Game(Base):
             except:
                 g = None
         return g
+
+    def update(self, other):
+        for attr in self.STANDARD_ATTRS:
+            setattr(self, attr, getattr(other, attr))
+
+    def __eq__(self, other):
+        return (
+            (
+                self.season, self.type, self.game_id, self.start, self.end,
+                self.date.strftime("%B %d, %Y"), self.attendance, self.venue,
+                self.road_team_id, self.home_team_id, self.road_score,
+                self.home_score, self.road_overall_game_count,
+                self.home_overall_game_count, self.road_game_count,
+                self.home_game_count) == (
+                other.season, other.type, other.game_id, other.start,
+                other.end, other.date.strftime("%B %d, %Y"),
+                other.attendance, other.venue, other.road_team_id,
+                other.home_team_id, other.road_score, other.home_score,
+                other.road_overall_game_count, other.home_overall_game_count,
+                other.road_game_count, other.home_game_count
+            ))
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __gt__(self, other):
+        return self.game_id > other.game_id
+
+    def __lt__(self, other):
+        return self.game_id < other.game_id
 
     def __str__(self):
         road_team = Team.find_by_id(self.road_team_id)
