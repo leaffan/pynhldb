fb399f00b93ae1f7bce2546511dbaa313a521f63
leaffan <leaffan@gmx.net>
2017-03-08 15:43:23 +0100

    Add find method to player game item

--- db/player_game.py
+++ db/player_game.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .common import Base
+from sqlalchemy import and_
+
+from .common import Base, session_scope
 from .player import Player
 
 
@@ -12,7 +14,7 @@ class PlayerGame(Base):
     STANDARD_ATTRS = [
         "position", "no", "goals", "assists", "primary_assists",
         "secondary_assists", "points", "plus_minus", "penalties", "pim",
-        "toi_overall", "toi_pp", "toi_sh", "toi_ev", "avg_shift", "no_shifts"
+        "toi_overall", "toi_pp", "toi_sh", "toi_ev", "avg_shift", "no_shifts",
         "shots_on_goal", "shots_blocked", "shots_missed", "hits",
         "giveaways", "takeaways", "blocks", "faceoffs_won", "faceoffs_lost",
         "on_ice_shots_on_goal", "on_ice_shots_blocked", "on_ice_shots_missed"
@@ -20,12 +22,26 @@ class PlayerGame(Base):
 
     def __init__(self, plr_game_id, game_id, team_id, plr_id, data_dict):
         self.player_game_id = plr_game_id
+        self.player_game_id_2 = int("%d%02d%d" % (game_id, team_id, plr_id))
         self.game_id = game_id
         self.team_id = team_id
         self.player_id = plr_id
         for attr in self.STANDARD_ATTRS:
             if attr in data_dict:
                 setattr(self, attr, data_dict[attr])
+
+    @classmethod
+    def find(self, game_id, player_id):
+        with session_scope() as session:
+            try:
+                plr_game = session.query(PlayerGame).filter(
+                    and_(
+                        PlayerGame.game_id == game_id,
+                        PlayerGame.player_id == player_id
+                    )).one()
+            except:
+                plr_game = None
+            return plr_game
 
     def __str__(self):
         if not hasattr(self, 'player') or self.player is None:
