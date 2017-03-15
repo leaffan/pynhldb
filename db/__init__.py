143af64f9435b3964ee618cccb89e7ad211e030a
leaffan <leaffan@gmx.net>
2017-03-15 04:44:34 +0100

    Add utility function to create or update database items

--- db/__init__.py
+++ db/__init__.py
@@ -8,3 +8,23 @@ def commit_db_item(db_item):
     with session_scope() as session:
         session.merge(db_item)
         session.commit()
+
+
+def create_or_update_db_item(db_item, new_item):
+    """
+    Updates an existing or creates a new database item.
+    """
+    with session_scope() as session:
+        # if database item exists
+        if db_item is not None:
+            # returning if database item is unchanged 
+            if db_item == new_item:
+                return
+            # updating database item otherwise
+            else:
+                db_item.update(new_item)
+                session.merge(db_item)
+        # creating database item otherwise
+        else:
+            session.add(new_item)
+        session.commit()
