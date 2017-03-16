bd8555598b8e361150726f359a11c99d4a084068
leaffan <leaffan@gmx.net>
2017-03-16 10:03:21 +0100

    Merge game parser tests

--- tests/test_game_parser.py
+++ tests/test_game_parser.py
@@ -5,7 +5,46 @@ import os
 import requests
 from lxml import html
 
+from parsers.team_parser import TeamParser
 from parsers.game_parser import GameParser
+
+
+def test_2016():
+
+    url = "http://www.nhl.com/scores/htmlreports/20162017/ES020776.HTM"
+
+    game_id = str(os.path.splitext(os.path.basename(url))[0][2:])
+
+    tp = TeamParser(get_data(url))
+    tp.load_data()
+    tp.create_teams()
+
+    gp = GameParser(game_id, get_data(url), None)
+    gp.load_data()
+    gp.create_game(tp.teams)
+
+    assert gp.game_data == [
+        'Saturday, February 4, 2017', 'Attendance 19,092 at Amalie Arena',
+        'Start 7:08 EST; End 10:09 EST', 'Game 0776', 'Final']
+
+
+def test_playoff_game():
+
+    url = "http://www.nhl.com/scores/htmlreports/20122013/GS030325.HTM"
+
+    game_id = str(os.path.splitext(os.path.basename(url))[0][2:])
+
+    tp = TeamParser(get_data(url))
+    tp.load_data()
+    tp.create_teams()
+
+    gp = GameParser(game_id, get_data(url), None)
+    gp.load_data()
+    gp.create_game(tp.teams)
+
+    assert gp.game_data == [
+        'Saturday, June 8, 2013', 'Attendance 22,237 at United Center',
+        'Start 7:20 CDT; End 11:02 CDT', 'Game 0325', 'Final']
 
 
 def test_2013_centre_bell():
