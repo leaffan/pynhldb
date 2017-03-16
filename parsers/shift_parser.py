6f6380bd0673f8e521bf4b430f00befd5727a530
leaffan <leaffan@gmx.net>
2017-03-16 15:29:51 +0100

    Finalize shift parser implementation

--- parsers/shift_parser.py
+++ parsers/shift_parser.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from collections import defaultdict
 
 from lxml import etree
 
@@ -21,37 +22,57 @@ class ShiftParser():
     def __init__(self, raw_data):
         self.raw_data = raw_data
         self.shift_data = dict()
+        self.shifts = defaultdict(list)
 
     def create_shifts(self, game, roster):
-
+        """
+        Creates shifts in specified game for given roster.
+        """
         self.load_data()
 
         # retrieving team
         team_name = self.raw_data.xpath(
             "//td[@class='teamHeading + border']/text()")[0]
         team = Team.find_by_name(team_name)
-        print(team)
 
         for no in sorted(self.shift_data.keys()):
+            # retrieving player with jersey number
             player = roster[no].get_player()
-            print(player.name)
+            # retrieving all shifts for current player
             shifts = self.get_shifts_for_player(self.shift_data[no], player)
+
+            # converting each single shift into a database item
             for shift_data_dict in shifts:
                 shift_data_dict['no'] = no
+
+                # setting up new shift item
                 shift = Shift(
                     game.game_id, team.team_id, player.player_id,
                     shift_data_dict)
+
+                # trying to find shift item in database
                 db_shift = Shift.find(
                     game.game_id, player.player_id,
                     shift_data_dict['in_game_shift_cnt'])
+
+                # creating new or updating existing shift item
                 create_or_update_db_item(db_shift, shift)
 
+                self.shifts[no].append(
+                    Shift.find(
+                        shift.game_id, shift.player_id,
+                        shift.in_game_shift_cnt))
+        else:
+            return self.shifts
+
     def get_shifts_for_player(self, shift_data_trs, player):
-
+        """
+        Gets all shifts in a game for a single player.
+        """
+        # setting up list of shifts
         shifts = list()
-
         for tr in shift_data_trs:
-
+            # setting up data dictionary for single shift
             shift = dict()
 
             tokens = tr.xpath("td/text()")
@@ -63,10 +84,8 @@ class ShiftParser():
                 shift['period'] = 4
             else:
                 shift['period'] = int(tokens[1])
-
             # retrieving shift start time
             shift['start'] = str_to_timedelta(tokens[2].split("/")[0].strip())
-
             # retrieving shift end time
             try:
                 shift['end'] = str_to_timedelta(
@@ -78,14 +97,12 @@ class ShiftParser():
                     "data: %s (game id: %s, %s)" % (
                         tokens[3], self.game.game_id, player.name))
                 shift['end'] = None
-
             # retrieving shift duration
             shift['duration'] = str_to_timedelta(tokens[4])
-
-            # if necessary calculate end time from start time and duration
+            # if necessary calculating end time from start time and duration
             if shift['end'] is None:
                 shift['end'] = shift['start'] + shift['duration']
-
+            # appending single shift to list of all shifts
             shifts.append(shift)
 
         return shifts
