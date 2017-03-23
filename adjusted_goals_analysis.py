3d61570023420f555f17f416c22bdac6426b1fce
leaffan <leaffan@gmx.net>
2017-03-23 08:32:22 +0100

    Integrate retrieval of top goalscorers per season

--- adjusted_goals_analysis.py
+++ adjusted_goals_analysis.py
@@ -8,7 +8,6 @@ import argparse
 from analysis._goals_per_game import retrieve_goals_per_season
 from analysis._goals_per_game import calculate_adjustment_factors
 from analysis._goal_leaders import retrieve_career_leaders
-from analysis._goal_leaders import retrieve_yearly_leaders
 from analysis._goal_leaders import retrieve_yearly_top
 from analysis._adjust_goals import retrieve_and_adjust_goal_totals
 
@@ -26,14 +25,14 @@ if __name__ == '__main__':
         help='Processing step(s) to conduct.',
         choices=['1', '2', '3', 'all'])
     # TODO: add arguments for goal scoring leader retrieval, i.e. maximum top
-    # threshold or minimum career season total
+    # position threshold or minimum career season total
 
     args = parser.parse_args()
     setup_steps = args.steps
 
     goals_per_season_path = os.path.join(
         "analysis", "goals_per_season.json")
-    career_goal_leaders_path = os.path.join(
+    goal_leaders_path = os.path.join(
         "analysis", "career_goal_leaders.json")
     adjusted_goal_data_path = os.path.join(
         "analysis", "adjusted_goal_data.json")
@@ -48,20 +47,30 @@ if __name__ == '__main__':
 
     # retrieving goal scoring leaders
     if setup_steps in ['2', 'all']:
-        career_goal_leaders = dict()
-        yearly_top = dict()
+        career_goal_leaders = list()
+        yearly_top = list()
+        # retrieving all players with at least 300 career goals
         career_goal_leaders = retrieve_career_leaders(300)
-        # yearly_top = retrieve_yearly_top(8, 2013, 2015)
+        # retrieving top five goalscorers per season
+        yearly_top = retrieve_yearly_top(5)
 
-        goal_leaders = {**career_goal_leaders, **yearly_top}  # noqa: E999
+        # retrieving urls to player pages for goal-scoring career leaders
+        career_leaders_urls = [d['url'] for d in career_goal_leaders]
+        # creating new list of all goal-scoring leaders
+        goal_leaders = career_goal_leaders
+        # adding goal-scoring per season leaders to list of all goal-scoring
+        # leaders if they are not yet a part of it
+        for yearly_leader in yearly_top:
+            if yearly_leader['url'] not in career_leaders_urls:
+                goal_leaders.append(yearly_leader)
 
-        open(career_goal_leaders_path, 'w').write(
+        open(goal_leaders_path, 'w').write(
             json.dumps(goal_leaders, indent=2))
 
     # adjusting goal scoring totals according to goals scored per season
     if setup_steps in ['3', 'all']:
         adjusted_goal_data = retrieve_and_adjust_goal_totals(
-            career_goal_leaders_path, goals_per_season_path)
+            goal_leaders_path, goals_per_season_path)
 
         open(adjusted_goal_data_path, 'w').write(
             json.dumps(adjusted_goal_data, sort_keys=True, indent=2))
