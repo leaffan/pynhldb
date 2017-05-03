f0a6060cb823bfe7cfbeb588422e754785a6f499
leaffan <leaffan@gmx.net>
2017-05-03 12:49:34 +0200

    Add score_against to list of standard attributes

--- db/team_game.py
+++ db/team_game.py
@@ -12,16 +12,17 @@ class TeamGame(Base):
     __autoload__ = True
 
     STANDARD_ATTRS = [
-        "score", "goals_for_1st", "goals_against_1st", "goals_for_2nd",
-        "goals_against_2nd", "goals_for_3rd", "goals_against_3rd", "goals_for",
-        "goals_against", "win", "regulation_win", "overtime_win",
-        "shootout_win", "loss", "regulation_loss", "overtime_loss",
-        "shootout_loss", "tie", "pp_overall", "pp_time_overall", "pp_5v4",
-        "pp_time_5v4", "pp_4v3", "pp_time_4v3", "pp_5v3", "pp_time_5v3",
-        "shots_for", "shots_against", "shots_for_1st", "shots_against_1st",
-        "shots_for_2nd", "shots_against_2nd", "shots_for_3rd",
-        "shots_against_3rd", "shots_for_ot", "shots_against_ot", "so_attempts",
-        "so_goals", "penalties", "pim", "points", "home_road_type"
+        "score", "score_against", "goals_for_1st", "goals_against_1st",
+        "goals_for_2nd", "goals_against_2nd", "goals_for_3rd",
+        "goals_against_3rd", "goals_for", "goals_against", "win",
+        "regulation_win", "overtime_win", "shootout_win", "loss",
+        "regulation_loss", "overtime_loss", "shootout_loss", "tie",
+        "pp_overall", "pp_time_overall", "pp_5v4", "pp_time_5v4", "pp_4v3",
+        "pp_time_4v3", "pp_5v3", "pp_time_5v3", "shots_for", "shots_against",
+        "shots_for_1st", "shots_against_1st", "shots_for_2nd",
+        "shots_against_2nd", "shots_for_3rd", "shots_against_3rd",
+        "shots_for_ot", "shots_against_ot", "so_attempts", "so_goals",
+        "penalties", "pim", "points", "home_road_type"
     ]
 
     def __init__(self, game_id, team_id, data_dict):
@@ -58,7 +59,7 @@ class TeamGame(Base):
         # comparing each standard attribute value (and game and team id) of
         # this object with other one's
         return (
-            [self.game_id, self.team_id] + 
+            [self.game_id, self.team_id] +
             [getattr(self, attr) for attr in self.STANDARD_ATTRS]
         ) == (
             [other.game_id, other.team_id] +
