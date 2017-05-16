c2cfb617d9bedf93e2c6dfb5ff6cdfcd35d5c0fe
leaffan <leaffan@gmx.net>
2017-05-16 23:40:06 +0200

    Add method to find by event and player id

--- db/shot_attempt.py
+++ db/shot_attempt.py
@@ -3,7 +3,9 @@
 
 import uuid
 
-from db.common import Base
+from sqlalchemy import and_
+
+from db.common import Base, session_scope
 from db.specific_event import SpecificEvent
 
 
@@ -29,3 +31,17 @@ class ShotAttempt(Base, SpecificEvent):
                 setattr(self, attr, False)
             else:
                 setattr(self, attr, None)
+
+    @classmethod
+    def find_by_event_player_id(self, event_id, player_id):
+        with session_scope() as session:
+            try:
+                shot_attempt = session.query(ShotAttempt).filter(
+                    and_(
+                        ShotAttempt.event_id == event_id,
+                        ShotAttempt.player_id == player_id
+                    )
+                ).one()
+            except:
+                shot_attempt = None
+            return shot_attempt
