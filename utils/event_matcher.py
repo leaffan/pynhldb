4b638b265811fa4965db2144602acb4d523252a3
leaffan <leaffan@gmx.net>
2017-05-18 23:30:51 +0200

    Add matching for simultaneous goals, shots and misses

--- utils/event_matcher.py
+++ utils/event_matcher.py
@@ -4,6 +4,8 @@
 import logging
 
 from db.event import Event
+from db.shot import Shot
+from db.shootout_attempt import ShootoutAttempt
 
 logger = logging.getLogger(__name__)
 
@@ -31,10 +33,10 @@ def is_matching_event(play, specific_event):
         return is_matching_hit_block_faceoff_event(specific_event, play)
     elif play['play_type'] in ['GIVE', 'TAKE']:
         return is_matching_giveaway_takeaway_event(specific_event, play)
-    # elif event.type in ['SHOT', 'GOAL']:
-    #     return is_matching_shot_event(play, event)
-    # elif event.type == 'MISS':
-    #     return is_matching_miss_event(play, event)
+    elif play['play_type'] in ['SHOT', 'GOAL']:
+        return is_matching_shot_event(specific_event, play)
+    elif play['play_type'] == 'MISS':
+        return is_matching_miss_event(specific_event, play)
 
     return False
 
@@ -118,3 +120,50 @@ def is_matching_giveaway_takeaway_event(specific_event, play):
         return True
     else:
         return False
+
+
+def is_matching_shot_event(specific_event, play):
+    """
+    Tries to match given (regular/shootout shot) event with specified play
+    retrieved from json data. This includes shots that turned into goals.
+    """
+    # retrieving base event
+    event = Event.find_by_id(specific_event.event_id)
+    # if it's a goal we have to retrieve the accompanying shot separately
+    if event.type == 'GOAL':
+        # it could be a shot in a shootout, too
+        if play['period_type'] == 'SHOOTOUT':
+            specific_event = ShootoutAttempt.find_by_event_id(event.event_id)
+        else:
+            specific_event = Shot.find_by_event_id(event.event_id)
+
+    if (
+        play['active'], play['shot_type']
+    ) == (
+        specific_event.player_id, specific_event.shot_type.lower()
+    ):
+        return True
+    else:
+        return False
+
+
+def is_matching_miss_event(specific_event, play):
+    """
+    Tries to match given (regular/shootout shot) event with specified play
+    retrieved from json data.
+    """
+    # retrieving base event
+    event = Event.find_by_id(specific_event.event_id)
+
+    if play['period_type'] == 'SHOOTOUT':
+        specific_event = ShootoutAttempt.find_by_event_id(event.event_id)
+
+    if specific_event.miss_type:
+        if specific_event.miss_type in play['description'] and \
+                specific_event.player_id == play['active']:
+            return True
+    else:
+        if specific_event.player_id == play['active']:
+            return True
+
+    return False
