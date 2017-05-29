fbaf1a64621e8b72b7ee46b8c58a12ed96a0f41f
leaffan <leaffan@gmx.net>
2017-05-29 23:58:52 +0200

    Add function to find downloadable files

--- utils/summary_downloader.py
+++ utils/summary_downloader.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from dateutil.parser import parse
-from dateutil.relativedelta import DAILY
-from dateutil.rrule import rrule
+from dateutil.rrule import rrule, DAILY
 
 
 class SummaryDownloader():
@@ -16,9 +15,11 @@ class SummaryDownloader():
 
     MAX_DOWNLOAD_WORKERS = 8
 
-    def __init__(self, tgt_dir, date, to_date='', threads=0):
+    def __init__(self, tgt_dir, date, to_date='', workers=0):
 
+        # parsing start date for summary retrieval
         self.date = parse(date)
+        # retrieving end date for summary retrieval
         if to_date:
             self.to_date = parse(to_date)
         else:
@@ -26,11 +27,21 @@ class SummaryDownloader():
         # preparing list of dates to download summary data for
         self.game_dates = list(
             rrule(DAILY, dtstart=self.date, until=self.to_date))
-        print(self.game_dates)
+
+        if workers:
+            self.MAX_DOWNLOAD_WORKERS = workers
+
+    def find_files_to_download(self):
+        """
+        Identifies files to be downloaded.
+        """
+        # making sure that the list of files to download is empty
+        self.files_to_download = list()
 
 
 if __name__ == '__main__':
 
     date = "1997/04/20"
+    to_date = "1998/01/31"
 
-    d = SummaryDownloader(r"d:\tmp", date)
+    d = SummaryDownloader(r"d:\tmp", date, to_date)
