0efa556ccb7dfa2282de2d3bfa701415396b913d
leaffan <leaffan@gmx.net>
2017-06-02 15:58:59 +0200

    Adjust to api changes

--- download_summaries.py
+++ download_summaries.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
 
     # setting target dir and time interval of interest
     tgt_dir = r"D:\nhl\official_and_json\2016-17"
+    tgt_dir = r"d:\tmp\test"
 
-    date = "May 20, 2017"
-    to_date = "May 30, 2017"
+    date = "2017/04/03"
+    to_date = "2017/04/03"
 
-    downloader = SummaryDownloader(tgt_dir, date, to_date)
-    # downloader.run()
+    downloader = SummaryDownloader(tgt_dir, date, to_date, workers=1)
+    downloader.run()
