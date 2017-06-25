162af20184a4c0d977c8c756fa1b9a711241a388
leaffan <leaffan@gmx.net>
2017-06-25 23:51:35 +0200

    Add additional tests for methods to find a player

--- tests/test_player.py
+++ tests/test_player.py
@@ -4,9 +4,36 @@
 from db.player import Player
 
 
+def test_constructor():
+    plr = Player(99, "Gretzky", "Wayne", "C", alternate_last_names=['Bruce'])
+    assert plr.player_id == 99
+    assert plr.first_name == "Wayne"
+    assert plr.last_name == "Gretzky"
+    assert plr.name == "Wayne Gretzky"
+    assert plr.position == "C"
+    assert plr.alternate_last_names == ['Bruce']
+    assert hasattr(plr, "alternate_first_names") is True
+    assert plr.alternate_first_names is None
+
+
 def test_find_by_id():
     plr = Player.find_by_id(8459469)
     assert plr.name == "Rory Fitzpatrick"
+
+
+def test_find_by_name():
+    plr = Player.find_by_name("Jaromir", "Jagr")
+    assert plr.player_id == 8448208
+
+
+def test_find_by_name_position():
+    plr = Player.find_by_name_position("Sidney", "Crosby", "C")
+    assert plr.player_id == 8471675
+
+
+def test_find_by_name_extended():
+    plr = Player.find_by_name_extended("Mitch", "Marner")
+    assert plr.player_id == 8478483
 
 
 def test_name_property():
