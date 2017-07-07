f715d565ea9274505ae31aeefd145a7dc921dd8a
leaffan <leaffan@gmx.net>
2017-07-07 17:03:26 +0200

    Move setup of comparison files to separate method

--- tests/test_summary_downloader.py
+++ tests/test_summary_downloader.py
@@ -11,21 +11,9 @@ from utils.summary_downloader import SummaryDownloader
 
 def test_download_unzipped():
 
-    date = "Oct 24, 2016"
-    tgt_dir = tempfile.mkdtemp(prefix='sdl_test_')
-    prefixes = ["ES", "FC", "GS", "PL", "RO", "SS", "TH", "TV"]
-    game_ids = ["020081", "020082"]
+    base_tgt_dir, date, files = set_up_comparison_files()
 
-    # setting up list of all HTML report files that should be downloaded for
-    # specified date
-    files = ["".join(c) + ".HTM" for c in list(
-        itertools.product(prefixes, game_ids))]
-    # adding JSON game report files
-    files.extend(["".join((gid, ".json")) for gid in game_ids])
-    # adding shootout report for one of the games
-    files.append("SO020082.HTM")
-
-    sdl = SummaryDownloader(tgt_dir, date, zip_summaries=False)
+    sdl = SummaryDownloader(base_tgt_dir, date, zip_summaries=False)
     sdl.run()
     tgt_dir = sdl.get_tgt_dir()
 
@@ -34,8 +22,21 @@ def test_download_unzipped():
 
 def test_download_zipped():
 
-    date = "Oct 24, 2016"
+    tgt_dir, date, files = set_up_comparison_files()
+
+    sdl = SummaryDownloader(tgt_dir, date)
+    sdl.run()
+    zip_path = sdl.get_zip_path()
+
+    zip = ZipFile(zip_path)
+
+    assert sorted(zip.namelist()) == sorted(files)
+
+
+def set_up_comparison_files():
+
     tgt_dir = tempfile.mkdtemp(prefix='sdl_test_')
+    date = "Oct 24, 2016"
     prefixes = ["ES", "FC", "GS", "PL", "RO", "SS", "TH", "TV"]
     game_ids = ["020081", "020082"]
 
@@ -48,10 +49,4 @@ def test_download_zipped():
     # adding shootout report for one of the games
     files.append("SO020082.HTM")
 
-    sdl = SummaryDownloader(tgt_dir, date)
-    sdl.run()
-    zip_path = sdl.get_zip_path()
-
-    zip = ZipFile(zip_path)
-
-    assert sorted(zip.namelist()) == sorted(files)
+    return tgt_dir, date, files
