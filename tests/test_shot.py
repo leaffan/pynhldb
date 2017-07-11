944ea4507897fa0f022838275be47a84e2400927
leaffan <leaffan@gmx.net>
2017-07-11 14:18:25 +0200

    Finalize actual test case for shot item

--- tests/test_shot.py
+++ tests/test_shot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 from lxml import html
 import tempfile
 
@@ -14,7 +15,7 @@ from parsers.event_parser import EventParser
 TMP_DIR = tempfile.mkdtemp(prefix='shot_test_')
 
 
-def setup_game_teams_rosters():
+def test_shot():
 
     date = "Oct 12, 2016"
     game_id = "020001"
@@ -23,32 +24,56 @@ def setup_game_teams_rosters():
     sdl.run()
     dld_dir = sdl.get_tgt_dir()
 
-    game_report_doc = html.fromstring(get_document(dld_dir, game_id, 'GS'))
-    roster_report_doc = html.fromstring(get_document(dld_dir, game_id, 'ES'))
-    play_by_play_report_doc = html.fromstring(
-        get_document(dld_dir, game_id, 'PL'))
-    game_feed_json_doc = get_json_document(dld_dir, game_id)
+    ep = get_event_parser(dld_dir, game_id)
+    event = ep.get_event(ep.event_data[7])
+    shot = ep.specify_event(event)
 
-    tp = TeamParser(game_report_doc)
-    teams = tp.create_teams()
-    gp = GameParser(game_id, game_report_doc)
-    game = gp.create_game(teams)
-    rp = RosterParser(roster_report_doc)
-    rosters = rp.create_roster(game, teams)
-    ep = EventParser(play_by_play_report_doc, game_feed_json_doc)
-    ep.load_data()
-    (ep.game, ep.rosters) = (game, rosters)
-#     event_data_item = ep.event_data[7]
-#     event = ep.get_event(event_data_item)
-#     shot = ep.specify_event(event)
-#     assert shot.team_id == 12
+    assert shot.event_id == event.event_id
+    assert shot.team_id == 10
+    assert shot.player_id == 8478483
+    assert shot.shot_type == 'Wrist'
+    assert shot.distance == 13
+    assert shot.zone == 'Off'
+    assert shot.goalie_id == 8467950
+    assert shot.goalie_team_id == 9
+    assert not shot.scored
 
 
 def get_document(dir, game_id, prefix):
     dh = DataHandler(dir)
-    return dh.get_game_data(game_id, prefix)
+    return open(dh.get_game_data(game_id, prefix)[prefix]).read()
 
 
 def get_json_document(dir, game_id):
     dh = DataHandler(dir)
-    return dh.get_game_json_data(game_id)
+    return json.loads(open(dh.get_game_json_data(game_id)).read())
+
+
+def get_event_parser(dir, game_id):
+    """
+    Retrieves event parser for game with specified id from
+    data downloaded to directory also given.
+    """
+    # retrieving raw data
+    game_report_doc = html.fromstring(get_document(dir, game_id, 'GS'))
+    roster_report_doc = html.fromstring(get_document(dir, game_id, 'ES'))
+    play_by_play_report_doc = html.fromstring(
+        get_document(dir, game_id, 'PL'))
+    game_feed_json_doc = get_json_document(dir, game_id)
+
+    # using team parser to retrieve teams
+    tp = TeamParser(game_report_doc)
+    teams = tp.create_teams()
+    # using game parser to retrieve basic game information
+    gp = GameParser(game_id, game_report_doc)
+    game = gp.create_game(teams)
+    # using roster parser to retrieve team rosters
+    rp = RosterParser(roster_report_doc)
+    rosters = rp.create_roster(game, teams)
+
+    # using event parser to retrieve all raw events
+    ep = EventParser(play_by_play_report_doc, game_feed_json_doc)
+    ep.load_data()
+    (ep.game, ep.rosters) = (game, rosters)
+    ep.cache_plays_with_coordinates()
+    return ep
