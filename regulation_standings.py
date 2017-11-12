b0cbd0b0d84466c4e92cae1032db1ed3658fc1cb
leaffan <leaffan@gmx.net>
2017-11-12 23:08:05 +0100

    Re-structure code

--- regulation_standings.py
+++ regulation_standings.py
@@ -56,6 +56,62 @@ def get_teams_by_division_conference(divisions):
     return teams_by_division, teams_by_conference
 
 
+def retrieve_standings(team_games):
+    standings = dict()
+
+    for tg in team_games:
+        if tg.team_id not in standings:
+            standings[tg.team_id] = defaultdict(int)
+        # aggregating games played
+        standings[tg.team_id]['gp'] += 1
+        # aggregating official goals for and against
+        standings[tg.team_id]['ogf'] += tg.score
+        standings[tg.team_id]['oga'] += tg.score_against
+        # aggregating actual goals for and against
+        standings[tg.team_id]['gf'] += tg.goals_for
+        standings[tg.team_id]['ga'] += tg.goals_against
+        # aggregating official wins and losses
+        standings[tg.team_id]['ow'] += tg.win
+        standings[tg.team_id]['ol'] += tg.regulation_loss
+        if any([tg.shootout_loss, tg.overtime_loss]):
+            standings[tg.team_id]['ootl'] += 1
+        # aggregating regulation wins
+        standings[tg.team_id]['w'] += tg.regulation_win
+        # aggregating ties, i.e. games that went to overtime or shootout
+        if any([
+                tg.overtime_win, tg.shootout_win,
+                tg.overtime_loss, tg.shootout_loss]):
+                    standings[tg.team_id]['t'] += 1
+        # decreasing actual goals scored by one for an overtime win
+        if tg.overtime_win:
+            standings[tg.team_id]['gf'] -= 1
+        # decreasing actual goals allowed by one for an overtime loss
+        if tg.overtime_loss:
+            standings[tg.team_id]['ga'] -= 1
+        # decreasing actual goal totals by empty-net goals for and against
+        standings[tg.team_id]['gf'] -= tg.empty_net_goals_for
+        standings[tg.team_id]['ga'] -= tg.empty_net_goals_against
+        # calculating goal differentials
+        standings[tg.team_id]['ogd'] = (
+            standings[tg.team_id]['ogf'] - standings[tg.team_id]['oga'])
+        standings[tg.team_id]['gd'] = (
+            standings[tg.team_id]['gf'] - standings[tg.team_id]['ga'])
+        # calculating points
+        standings[tg.team_id]['opts'] = (
+            standings[tg.team_id]['ow'] * 2 + standings[tg.team_id]['ootl'])
+        standings[tg.team_id]['pts'] = (
+            standings[tg.team_id]['w'] * 2 + standings[tg.team_id]['t'])
+        # calculating point percentage
+        standings[tg.team_id]['oppctg'] = round(
+            standings[tg.team_id]['opts'] / (
+                standings[tg.team_id]['gp'] * 2.0), 3)
+        standings[tg.team_id]['ppctg'] = round(
+            standings[tg.team_id]['pts'] / (
+                standings[tg.team_id]['gp'] * 2.0), 3)
+
+    return standings
+
+
 if __name__ == '__main__':
 
     season = 2017
@@ -65,55 +121,7 @@ if __name__ == '__main__':
     # TODO
     # rank by division, conference etc.
 
-    summary = dict()
-
-    for tg in team_games:
-        if tg.team_id not in summary:
-            summary[tg.team_id] = defaultdict(int)
-        # aggregating games played
-        summary[tg.team_id]['gp'] += 1
-        # aggregating official goals for and against
-        summary[tg.team_id]['ogf'] += tg.score
-        summary[tg.team_id]['oga'] += tg.score_against
-        # aggregating actual goals for and against
-        summary[tg.team_id]['gf'] += tg.goals_for
-        summary[tg.team_id]['ga'] += tg.goals_against
-        # aggregating official wins and losses
-        summary[tg.team_id]['ow'] += tg.win
-        summary[tg.team_id]['ol'] += tg.regulation_loss
-        if any([tg.shootout_loss, tg.overtime_loss]):
-            summary[tg.team_id]['ootl'] += 1
-        # aggregating regulation wins
-        summary[tg.team_id]['w'] += tg.regulation_win
-        # aggregating ties, i.e. games that went to overtime or shootout
-        if any([
-                tg.overtime_win, tg.shootout_win,
-                tg.overtime_loss, tg.shootout_loss]):
-                    summary[tg.team_id]['t'] += 1
-        # decreasing actual goals scored by one for an overtime win
-        if tg.overtime_win:
-            summary[tg.team_id]['gf'] -= 1
-        # decreasing actual goals allowed by one for an overtime loss
-        if tg.overtime_loss:
-            summary[tg.team_id]['ga'] -= 1
-        # decreasing actual goal totals by empty-net goals for and against
-        summary[tg.team_id]['gf'] -= tg.empty_net_goals_for
-        summary[tg.team_id]['ga'] -= tg.empty_net_goals_against
-        # calculating goal differentials
-        summary[tg.team_id]['ogd'] = (
-            summary[tg.team_id]['ogf'] - summary[tg.team_id]['oga'])
-        summary[tg.team_id]['gd'] = (
-            summary[tg.team_id]['gf'] - summary[tg.team_id]['ga'])
-        # calculating points
-        summary[tg.team_id]['opts'] = (
-            summary[tg.team_id]['ow'] * 2 + summary[tg.team_id]['ootl'])
-        summary[tg.team_id]['pts'] = (
-            summary[tg.team_id]['w'] * 2 + summary[tg.team_id]['t'])
-        # calculating point percentage
-        summary[tg.team_id]['oppctg'] = round(
-            summary[tg.team_id]['opts'] / (summary[tg.team_id]['gp'] * 2.0), 3)
-        summary[tg.team_id]['ppctg'] = round(
-            summary[tg.team_id]['pts'] / (summary[tg.team_id]['gp'] * 2.0), 3)
+    standings = retrieve_standings(team_games)
 
     i = 1
 
@@ -126,20 +134,20 @@ if __name__ == '__main__':
     print("  # %-22s %2s %2s %2s %2s %3s %3s %3s %3s" % (
         'Team', 'GP', 'W', 'L', 'OT', 'Pts', 'GF', 'GA', 'GD'))
 
-    for team_id in sorted(summary, key=lambda x: (
-            summary[x]['opts'], summary[x]['oppctg'], summary[x]['ow'],
-            summary[x]['ogd'], summary[x]['ogf']), reverse=True):
+    for team_id in sorted(standings, key=lambda x: (
+            standings[x]['opts'], standings[x]['oppctg'], standings[x]['ow'],
+            standings[x]['ogd'], standings[x]['ogf']), reverse=True):
         team = Team.find_by_id(team_id)
-        gd, fore = get_colored_output(summary[team_id]['ogd'])
+        gd, fore = get_colored_output(standings[team_id]['ogd'])
 
         # saving official ranking
-        summary[team_id]['orank'] = i
+        standings[team_id]['orank'] = i
 
         print("%3d %-22s %2d %2d %2d %2d %3d %3d-%3d %s%s%s" % (
-            i, team, summary[team_id]['gp'], summary[team_id]['ow'],
-            summary[team_id]['ol'], summary[team_id]['ootl'],
-            summary[team_id]['opts'],
-            summary[team_id]['ogf'], summary[team_id]['oga'],
+            i, team, standings[team_id]['gp'], standings[team_id]['ow'],
+            standings[team_id]['ol'], standings[team_id]['ootl'],
+            standings[team_id]['opts'],
+            standings[team_id]['ogf'], standings[team_id]['oga'],
             fore, gd, Style.RESET_ALL))
         i += 1
 
@@ -151,18 +159,18 @@ if __name__ == '__main__':
 
     print("  # (O#) %-22s %2s %2s %2s %2s %3s %3s %3s %3s" % (
         'Team', 'GP', 'W', 'L', 'T', 'Pts', 'GF', 'GA', 'GD'))
-    for team_id in sorted(summary, key=lambda x: (
-            summary[x]['pts'], summary[x]['ppctg'], summary[x]['w'],
-            summary[x]['gd'], summary[x]['gf']), reverse=True):
+    for team_id in sorted(standings, key=lambda x: (
+            standings[x]['pts'], standings[x]['ppctg'], standings[x]['w'],
+            standings[x]['gd'], standings[x]['gf']), reverse=True):
         team = Team.find_by_id(team_id)
         # determining output format for goal differential
-        gd, fore = get_colored_output(summary[team_id]['gd'])
+        gd, fore = get_colored_output(standings[team_id]['gd'])
 
         print("%3d (%2d) %-22s %2d %2d %2d %2d %3d %3d-%3d %s%s%s" % (
-            i, summary[team_id]['orank'], team, summary[team_id]['gp'],
-            summary[team_id]['w'], summary[team_id]['ol'],
-            summary[team_id]['t'], summary[team_id]['pts'],
-            summary[team_id]['gf'], summary[team_id]['ga'],
+            i, standings[team_id]['orank'], team, standings[team_id]['gp'],
+            standings[team_id]['w'], standings[team_id]['ol'],
+            standings[team_id]['t'], standings[team_id]['pts'],
+            standings[team_id]['gf'], standings[team_id]['ga'],
             fore, gd, Style.RESET_ALL))
         i += 1
 
