f04b5010b8ad9aea55ca50c3a4ee174547dcbff5
Markus Reinhold <leaffan@gmx.net>
2018-01-14 01:50:09 +0100

    Fix imports, shorten imports to use abbreviation

--- _setup_database.py
+++ _setup_database.py
@@ -3,19 +3,10 @@
 
 import argparse
 
-from setup.create_teams import migrate_teams
-from setup.create_divisions import create_divisions
-from setup.create_players import migrate_players
-from setup.create_players import search_players
-from setup.create_players import create_players_for_draft_year
-from setup.create_player_data import create_player_seasons
-from setup.create_player_data import create_player_data
-from setup.create_player_data import create_player_contracts
-from setup.create_player_data import create_player_contracts_by_team
-from setup.create_player_data import create_player_drafts
-from setup.create_player_data import create_capfriendly_ids
-from setup.create_player_data import create_capfriendly_ids_by_team
-from setup.create_player_data import create_latest_contract_signings
+from setup import create_teams as ct
+from setup import create_divisions as cd
+from setup import create_players as cp
+from setup import create_player_data as cpd
 
 from utils import prepare_logging
 prepare_logging(log_types=['file', 'screen'])
@@ -48,40 +39,41 @@ if __name__ == '__main__':
 
     # migrating teams from json file to database
     if setup_steps in ['t', 'a']:
-        migrate_teams()
+        ct.migrate_teams()
     # creating divisions from division configuration file
     if setup_steps in ['d', 'a']:
-        create_divisions()
+        cd.create_divisions()
     # migrating players from json file to database
     if setup_steps in ['p', 'a']:
-        migrate_players()
+        cp.migrate_players()
     # finding players on roster/system/contract pages
     if setup_steps in ['pf', 'a']:
-        search_players(args.roster_src, args.roster_teams, args.roster_season)
+        cp.search_players(
+            args.roster_src, args.roster_teams, args.roster_season)
     # creating players from draft overview
     if setup_steps in ['pc', 'a']:
-        create_players_for_draft_year(args.draft_year)
+        cp.create_players_for_draft_year(args.draft_year)
     # retrieving player season statistics for all players in database
     if setup_steps in ['ps', 'a']:
-        create_player_seasons()
+        cpd.create_player_seasons()
     # retrieving individual player data for all players in database
     if setup_steps in ['pd', 'a']:
-        create_player_data()
+        cpd.create_player_data()
     # retrieving contract data for all players in database
     if setup_steps in ['c']:
-        create_player_contracts()
+        cpd.create_player_contracts()
     # retrieving contract data for all players contracted
     if setup_steps in ['ct']:
-        create_player_contracts_by_team()
+        cpd.create_player_contracts_by_team()
     # retrieving latest contract signings
     if setup_steps in ['lc']:
-        create_latest_contract_signings(5)
+        cpd.create_latest_contract_signings(5)
     # retrieving draft data for all players in database
     if setup_steps in ['dft']:
-        create_player_drafts()
+        cpd.create_player_drafts()
     # retrieving capfriendly ids for all players in database
     if setup_steps in ['cf']:
-        create_capfriendly_ids()
+        cpd.create_capfriendly_ids()
     # retrieving capfriendly ids for players of all teams in database
     if setup_steps in ['cft']:
-        create_capfriendly_ids_by_team()
+        cpd.create_capfriendly_ids_by_team()
