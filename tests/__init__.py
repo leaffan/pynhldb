d133f4cae9e291ca397a137bd4036170229124d2
Markus Reinhold <leaffan@gmx.net>
2023-09-02 19:09:06 +0200

    Add valid shot type

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 VALID_SHOT_TYPES = [
-    'Backhand', 'Bat', 'Between Legs', 'Deflected', 'Poke', 'Slap', 'Snap', 'Tip-In', 'Wrap-around', 'Wrist']
+    'Backhand', 'Bat', 'Between Legs', 'Deflected', 'Poke', 'Slap', 'Snap', 'Tip-In', 'Wrap-around', 'Wrist', 'Cradle']
 VALID_ZONES = ['Off', 'Def', 'Neu']
